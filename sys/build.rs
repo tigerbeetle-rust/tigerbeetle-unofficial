6243665210c598a6aa80ec411ac6bc7cfdfca913
tyranron <tyranron@gmail.com>
2024-11-19 15:35:17 +0100

    Debug Windows build

--- sys/build.rs
+++ sys/build.rs
@@ -36,8 +36,8 @@ fn target_to_tigerbeetle_target(target: &str) -> Option<&'static str> {
         "x86_64-unknown-linux-gnu" => Some("x86_64-linux"),
         "x86_64-unknown-linux-musl" => Some("x86_64-linux-musl"),
         "x86_64-apple-darwin" => Some("x86_64-macos"),
-        "x86_64-pc-windows-gnu" => Some("x86_64-windows"),
-        "x86_64-pc-windows-msvc" => Some("x86_64-windows"),
+        "x86_64-pc-windows-gnu" => Some("x86_64-windows-gnu"),
+        "x86_64-pc-windows-msvc" => Some("x86_64-windows-msvc"),
         _ => None,
     }
 }
@@ -118,6 +118,11 @@ fn main() {
         let c_dir = tigerbeetle_root.join("src/clients/c/");
         let lib_dir = tigerbeetle_root.join("src/clients/c/lib");
         let link_search = lib_dir.join(target_lib_subdir);
+        for entry in fs::read_dir(lib_dir).unwrap() {
+            let dir = entry.unwrap();
+            println!("{:?}", dir.path());
+        }
+        panic!("wut?");
         println!(
             "cargo:rustc-link-search=native={}",
             link_search
