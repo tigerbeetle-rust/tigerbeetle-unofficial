4bd7f91b56ff2b2c89592bbbb8c6d96d94905783
tyranron <tyranron@gmail.com>
2024-12-18 16:19:16 +0100

    Update implementation and tests to match upstream

--- src/id.rs
+++ src/id.rs
@@ -1,55 +1,154 @@
-use rand::RngCore;
-use std::time::{SystemTime, UNIX_EPOCH};
+use std::{sync::Mutex, time::UNIX_EPOCH};
 
-fn get_current_timestamp() -> u64 {
-    let start = SystemTime::now();
-    let since_the_epoch = start
-        .duration_since(UNIX_EPOCH)
-        .expect("Time went backwards");
-    since_the_epoch.as_millis() as u64
-}
-
-fn generate_random_bytes() -> [u8; 10] {
-    let mut rng = rand::thread_rng();
-    let mut random_bytes = [0u8; 10];
-    rng.fill_bytes(&mut random_bytes);
-    random_bytes
-}
-
-/// Generate a new unique ID suitable for use as an ID in TigerBeetle.
+/// Returns the current timestamp in milliseconds since [`UNIX_EPOCH`].
 ///
-/// https://docs.tigerbeetle.com/coding/data-modeling/#id
+/// # Panics
+///
+/// - If the [`SystemTime`] clock went backwards beyond [`UNIX_EPOCH`].
+/// - If milliseconds since [`UNIX_EPOCH`] overflow [`u64`].
+fn get_current_timestamp() -> u64 {
+    UNIX_EPOCH
+        .elapsed()
+        .unwrap_or_else(|e| panic!("`SystemTime` went backwards beyond `UNIX_EPOCH`: {e}"))
+        .as_millis()
+        .try_into()
+        .unwrap_or_else(|e| panic!("milliseconds since `UNIX_EPOCH` overflow `u64`: {e}"))
+}
+
+/// Generates and returns 10 random bytes.
+fn generate_random_bytes() -> [u8; 10] {
+    let mut bytes = [0u8; 10];
+    fastrand::fill(&mut bytes);
+    bytes
+}
+
+/// Generates a new [TigerBeetle Time-Based Identifier][0].
+///
+/// [TigerBeetle Time-Based Identifier][0] consists of:
+/// - 48 bits of (millisecond) timestamp (high-order bits)
+/// - 80 bits of randomness (low-order bits)
+///
+/// [0]: https://docs.tigerbeetle.com/coding/data-modeling#tigerbeetle-time-based-identifiers-recommended
+#[must_use]
 pub fn id() -> u128 {
-    let timestamp = get_current_timestamp();
+    static LAST: Mutex<(u64, [u8; 10])> = Mutex::new((0, [0; 10]));
 
-    let millis = &timestamp.to_le_bytes()[..6];
-    let random = generate_random_bytes();
+    let (timestamp, random) = {
+        let timestamp = get_current_timestamp();
 
+        // Lock the `Mutex` to ensure that `last_timestamp` is monotonically increasing and
+        // `last_random` changes each millisecond.
+        let (last_timestamp, last_random) = &mut *LAST.lock().unwrap();
+        if timestamp > *last_timestamp {
+            *last_timestamp = timestamp;
+            *last_random = generate_random_bytes();
+        }
+
+        // Read out a `u80` from the `last_random` as a `u64` and `u16`.
+        // PANIC: Unwrapping is OK here, since `mem::size_of<u64>() == 8` and
+        //        `mem::size_of<u16>() == 2`.
+        let mut random_lo = u64::from_le_bytes(last_random[..8].try_into().unwrap());
+        let mut random_hi = u16::from_le_bytes(last_random[8..].try_into().unwrap());
+
+        // Increment the random bits as a `u80` together, checking for overflow.
+        random_lo = random_lo.wrapping_add(1);
+        if random_lo == 0 {
+            random_hi = random_hi.wrapping_add(1);
+            if random_hi == 0 {
+                *last_timestamp = last_timestamp.wrapping_add(1);
+            }
+        }
+
+        // Write incremented `u80` back to the `last_random`.
+        last_random[..8].copy_from_slice(&random_lo.to_le_bytes());
+        last_random[8..].copy_from_slice(&random_hi.to_le_bytes());
+
+        (*last_timestamp, *last_random)
+    };
+
+    // Create `u128` from new `timestamp` and `random`.
     let mut id = [0u8; 16];
     id[0..10].copy_from_slice(&random);
-    id[10..16].copy_from_slice(millis);
-    // SAFETY: `id` is 16 bytes long
-    u128::from_le_bytes(id[..16].try_into().unwrap())
+    id[10..16].copy_from_slice(&timestamp.to_le_bytes()[..6]);
+    u128::from_le_bytes(id)
 }
 
 #[cfg(test)]
-mod tests {
-    use std::time::Duration;
+mod id_spec {
+    use std::{sync::Barrier, thread, time::Duration};
 
-    use super::*;
+    use super::id;
 
     #[test]
-    fn smoke() {
+    fn unique() {
         let id1 = id();
         let id2 = id();
-        assert_ne!(id1, id2);
+        assert_ne!(id1, id2, "expected: {id1} != {id2}");
     }
 
     #[test]
-    fn smoke_order() {
+    fn monotonic_between_millis() {
         let id1 = id();
-        std::thread::sleep(Duration::from_millis(2));
+        thread::sleep(Duration::from_millis(2));
         let id2 = id();
-        assert!(dbg!(id1) < dbg!(id2));
+        assert!(id1 < id2, "expected: {id1} < {id2}");
+    }
+
+    #[test]
+    fn monotonic_within_millis() {
+        let id1 = id();
+        thread::sleep(Duration::from_micros(1));
+        let id2 = id();
+        assert!(id1 < id2, "expected: {id1} < {id2}");
+    }
+
+    #[test]
+    fn monotonic_immediately() {
+        let id1 = id();
+        let id2 = id();
+        assert!(id1 < id2, "expected: {id1} < {id2}");
+    }
+
+    // Port of upstream test:
+    // https://github.com/tigerbeetle/tigerbeetle/blob/0.16.11/src/clients/go/pkg/types/main_test.go#L75-L115
+    #[test]
+    fn monotonic_fuzz() {
+        fn verifier() {
+            let mut id1 = id();
+            for i in 0..1_000_000 {
+                if i % 1_000 == 0 {
+                    thread::sleep(Duration::from_millis(1));
+                }
+                let id2 = id();
+
+                assert!(id1 < id2, "expected: {id1} < {id2}");
+
+                id1 = id2;
+            }
+        }
+
+        // Verify monotonic IDs locally.
+        verifier();
+
+        // Verify monotonic IDs across multiple threads.
+        let n = 10;
+        let barrier = Barrier::new(n);
+        thread::scope(|s| {
+            let threads = (0..n)
+                .map(|i| {
+                    thread::Builder::new()
+                        .name(i.to_string())
+                        .spawn_scoped(s, || {
+                            // Sync up all threads before `verifier()` to maximize contention.
+                            barrier.wait();
+                            verifier();
+                        })
+                        .unwrap()
+                })
+                .collect::<Vec<_>>();
+            for t in threads {
+                t.join().unwrap();
+            }
+        });
     }
 }
