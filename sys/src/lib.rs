f3ea694cf89d9e7a139a582ef9a89837aeae6f10
tyranron <tyranron@gmail.com>
2024-12-30 15:34:03 +0200

    Remove unnecessary `improper_ctypes` lint suppression (#38)

--- sys/src/lib.rs
+++ sys/src/lib.rs
@@ -5,7 +5,6 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
-#![allow(improper_ctypes)] // u128
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
